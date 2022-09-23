baa870f12a9bec462cba71381cd3a2553caa3dc3
Alvaro González <aggm123@icloud.com>
2022-09-22 17:08:45 -0700

    Worker model.

--- apps/worker/models.py
+++ apps/worker/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from apps.equipo.models import equipo
 
 # Create your models here.
+
+# |-----| cIUDADES REGISTRADAS
+POSICIONES = [
+    ('QUQU', 'Quarterbacks'),
+    ('RURU', 'Running backs'),
+    ('WIWI', 'Wide receivers'),
+    ('TITI', 'Tight ends'),
+    ('DEDE', 'Defensive linemen'),
+    ('LILI', 'Linebackers'),
+    ('DEDE', 'Defensive backs'),
+    ('SPSP', 'Special teams'),
+    ('PRPR', 'Practice squad'),
+]
+# |-----| Método de búsqueda de fotos
+def get_worker_image_filepath(instance, filename):
+    filebase, extension = filename.split('.')
+    return 'worker/worker_%s/profile_picture.%s' % (instance.workerUsername, extension)
+
+def get_default_worker_image():
+	return "worker/worker_worker_image.png"
+
+# |-----| Models para workeradores
+class worker(models.Model):
+    workerUsername = models.CharField(
+        max_length=64, verbose_name=u"Username", null=False)
+    workerNameFirst = models.CharField(
+        max_length=64, verbose_name=u"Firstname", null=False)
+    workerNameLast = models.CharField(
+        max_length=64, verbose_name=u"Lastname", null=False)
+    workerNickname = models.CharField(
+        max_length=64, verbose_name=u"Nickname", null=False)
+    workerEmail = models.EmailField(
+        max_length=60, verbose_name=u"Email")
+    workerDateRegistered = models.DateField(auto_now_add=True)
+    workerDateBirth = models.DateField(
+        verbose_name=u"BirthDate", null=False)
+
+    # Fotografía del trabajador
+    workerPhoto = models.ImageField(max_length=255, 
+        upload_to=get_worker_image_filepath, 
+        blank=True, default=get_default_worker_image,
+        verbose_name=u"Upload a picture of you")
+
+    # Referencia del usuario
+    user = models.OneToOneField(User, null=false, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s" % (self.workerUsername)
+    
+    class Meta:
+        ordering = ['workerNameLast']
+    
